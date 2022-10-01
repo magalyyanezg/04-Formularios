ecf7b4f7f39e8b3411e6d28b8b05f5d7a666611a
magalyyanezg <a3521110208@alumno.uttehuacan.edu.mx>
2022-09-30 23:36:55 -0500

    Primera versión 04-formularios 30/09/22

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container mt-3">
+    <Header />
+   
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Proyecto</label>
+        <input type="text" class="form-control" id="exampleInputProject" aria-describedby="projectHelp">
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Actividad</label>
+        <input type="text" class="form-control" id="exampleInputPassword1">
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Urgente</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Guardar</button>
+    </form>
+
+  </div>
+</template>
+
+<script>
+  
+  import Header from "./components/Header.vue"
+  
+  export default {
+    components: {
+      Header,
+      
+    }
+  }
+</script>
+
+<style scoped>
+  body {
+    margin: 0px 2rem;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container mt-3">
+        <div class="row">
+          <div class="col-12 col-md-6">
+            <h1>Formularios</h1>
+          </div>
+          <div class="col-12 col-md-6">
+            <img :src="url" :alt="alt" width="100">
+          </div>
+       </div>
+
+       
+        <hr />
+    </div>
+</template>
+
+<script>
+  //import Header from "./components/Header.vue"
+  import Formulario from "./Formulario.vue"
+    export default {
+      data:() => ( {
+      url:"https://cdn.svgporn.com/logos/vue.svg?response-content-disposition=attachment%3Bfilename%3Dvue.svg",
+      alt: "Vue.js",
+      nombre:"",
+      edad:0,
+      correo:"",  
+      Formulario,
+    }),
+  
+    };
+    
+  </script>
