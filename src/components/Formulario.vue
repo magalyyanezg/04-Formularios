ec2bccd765737ee881b061aca608a799898b04cf
magalyyanezg <a3521110208@alumno.uttehuacan.edu.mx>
2022-10-13 12:00:01 -0500

    actualizacion de examen

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,62 +1,66 @@
 <template>
-    
+
     <div class="row">
         <div class="col-12 mb-4">
-            <progress-bar :porcentaje="porcentaje" />
+            <ProgressBar :porcentaje="porcentaje">
+
+            </ProgressBar>
+            
         </div>
 
 
         <div class="col-12 col-md-5">
-        
-            <form @submit.prevent="resgistrarProyecto">
+            <form @submit.prevent="registrarproyecto">
+                <div class="mb-3">
+                <label  class="form-label">Proyecto</label>
+                <input v-model.trim="proyecto" type="text" class="form-control" required />
+                </div>
 
-                <div class="mb-3">
-                    <label class="form-label">Proyecto</label>
-                    <input v-model.trim="proyecto" type="text" class="form-control" required/>
-                </div>
-        
-                <div class="mb-3">
-                    <label class="form-label">Actividad</label>
-                    <select v-model.trim="tipo" class="form-select" required>
-                        <option disabled selected value=""> Selecciona un tipo de actividad</option>
-                        <option >Aplicaciones web con Vue.js</option>
-                        <option >Backend Services con Node.js</option>
-                        <option >App móvil con React Native</option>
-        
-                    </select>
-                </div>
-        
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-check-label">Urgente</label>
-                    <input v-model="urgente" type="checkbox" class="form-check-input">
-                </div>
-        
-                <button type="submit" class="btn btn-primary">Guardar</button>
-            </form>
+        <div class="mb-3">
+            <label  class="form-label">Actividad</label>
+            <select v-model="tipo" class="form-select" required>
+            <option disabled selected value="">Selecciona un tipo de actividad</option>
+            <option>Aplicaciones Wen con Vue.js</option>
+            <option>Backend Services con Node.js</option>
+            <option>App móvil con React Native</option>
+        </select>
+      </div>
 
-        
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-check-label">Urgente</label>
+        <input v-model="urgente" type="checkbox" class="form-check-input"/>
+      </div>
+
+
+      <button type="submit" class="btn btn-primary">Guardar</button>
+    </form>
         </div>
-        
+
         <div class="col-12 col-md-7">
             <total-proyectos :numeroProyectos="numeroProyectos" :proyectos="proyectos" :cambiarEstado="cambiarEstado" :limpiarData="limpiarData"/>
 
         </div>
+        
     </div>
+    
 </template>
 
 <script>
-    import ProgressBar from './ProgressBar.vue';
-    import TotalProyectos from './TotalProyectos.vue';
-        export default  {
-            data: () => ({
+
+import TotalProyectos from './TotalProyectos.vue';
+import ProgressBar from './ProgressBar.vue';
+import { onMounted } from 'vue';
+    export default{
+        data: () => ({
                 proyecto: "",
                 tipo: "",
                 urgente: false,
                 proyectos: [],
                 numeroProyectos: 0,
+                
             }),
             methods: {
-                resgistrarProyecto() {
+                registrarproyecto() {
                     const proyecto = {
                         proyecto: this.proyecto,
                         tipo: this.tipo,
@@ -64,31 +68,38 @@
                         completado: false,
                     };
                     this.proyectos.push(proyecto);
-        
+
                     this.saveData();
-        
-                    this.proyecto = "";
-                    this.tipo = "";
+
+                    this.numeroProyectos++;
+                    this.proyecto = "",
+                    this.tipo = "",
                     this.urgente = false;
-                },
-                cambiarEstado(proyecto, campo) {
-                    // this.proyectos[id].urgente = !this.proyectos[id].urgente;
-                    //console.log(proyecto);
-                    proyecto[campo] = !proyecto[campo];
-                    this.saveData();
-                    
-                },
-                saveData() {
-                    localStorage.setItem("proyectos", JSON.stringify(this.proyectos));
-                },
-                limpiarData() {
-                    this.proyectos = [];
-                    localStorage.clear();
-                },
+                
             },
+            cambiarEstado(proyecto,campo) {
+                //this.proyectos[id].urgente = !this.proyectos[id].urgente;
+                proyecto[campo] = !proyecto[campo];
+
+                this.saveData();
+                
+            },
+            saveData(){
+                localStorage.setItem("proyectos", JSON.stringify(this.proyectos));
+            },
+            
+                limpiarData() {
+                this.proyectos = [];
+                localStorage.clear;
+            },
+            remove: function(index) {
+                this.proyectos.splice(index, 1);
+                this.saveData();
+            }
+        },
             computed: {
                 numeroProyectos() {
-                    return this.proyectos.legth;
+                    return this.proyectos.length;
                 },
                 porcentaje() {
                     let completados = 0;
@@ -96,13 +107,17 @@
                         if (proyecto.completado)
                             completados++;
                     });
-                    return (completados * 100) / this.numeroProyectos || 0;
+                    return (completados * 100) / this.numeroProyectos || 0; 
                 },
             },
-            components: { ProgressBar, TotalProyectos},
-            mounted() {
-                this.proyectos = JSON.parse(localStorage.getItem("proyectos")) || [];
+            components: {
+                TotalProyectos,
+                ProgressBar
             },
-    
-        };
+            mounted() {
+                this.proyectos= JSON.parse(localStorage.getItem("proyectos")) || [];
+            },
+    };
+
 </script>
+
